6c2131a1d90bfd1ec38502099d3b5e71713dbd2b
raju ranjan <rajuranjansimari110@gmai.com>
2021-11-25 23:57:43 +0530

    first commit

---
+++ c++/2nd.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+ using namespace std;
+class first
+{
+public:
+  sum()
+  {
+    cout<<"hello raju";
+  }
+};
+
+class second
+{
+    public:
+    add()
+    {
+       cout<<"thank you"; 
+    }
+};
+int main()
+{
+    second o1;
+    o1.add()
+}
+
+
---
+++ hello-world.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    clrscr();
+    printf("hello Raju");
+    if (4>3)
+    {
+        printf("\n You are welcome");
+    }
+    
+    return 1;
+    getch();
+}
